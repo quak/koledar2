10f35ceefbd04da6a2aaf3e7206ced03794884bf
Augustin Malle <augustin.malle@gmail.com>
2024-02-06 13:47:12 +0100

    asdjkhasdkj

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,8 @@ let mix = require('laravel-mix');
 // Process our app.js file and output it to /dist/app.js
 mix.js('src/app.js', 'dist/app.js').setPublicPath('dist');
 
+mix.copy('src/images', 'dist/images');
+
 // Setup HTML-Loader to allow us to import HTML templates
 mix.webpackConfig({
   module: {
