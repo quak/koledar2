0b3c785be5818bf10a33b40ae4aec68b1d952184
Augustin Malle <augustin.malle@gmail.com>
2024-02-25 18:01:39 +0100

    knjm,

--- dist/app.js
+++ dist/app.js
@@ -3479,11 +3479,12 @@ var injectCSS = function injectCSS() {
   // Set the link type to and rel attributes
   link.type = "text/css";
   link.rel = "stylesheet";
-  var environment = 'productionTest';
+  var environment = 'production';
   //if (process.env.NODE_ENV  == 'production') {
   if (environment == 'production') {
     // A CDN link to your production CSS
-    link.href = "https://cdn.jsdelivr.net/gh/quak/koledar2@main/dist/styles-pro.css";
+    //link.href = "https://cdn.jsdelivr.net/gh/quak/koledar2@main/dist/styles-pro.css";
+    link.href = "https://cdn.jsdelivr.net/gh/quak/koledar2/dist/styles-pro.css";
   } else {
     // Your local CSS for local development
     link.href = "./../dist/styles-pro.css";
